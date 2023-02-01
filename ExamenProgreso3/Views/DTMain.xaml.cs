19ea2ad247328e023bb1a19b2afdbd14cf06b4eb
David Antonio Trujillo Robalino <david.trujillo.robalino@udla.edu.ec>
2023-02-01 08:43:39 -0500

    Creacion carpeta models con definicion y creacion carpte views para manejar la info de la API

---
+++ ExamenProgreso3/Views/DTMain.xaml.cs
@@ -0,0 +1,43 @@
+using System.Net;
+using ExamenProgreso3.Models;
+using Newtonsoft.Json;
+
+namespace ExamenProgreso3.Views;
+
+
+
+public partial class DTMain : ContentPage
+{
+	public DTMain()
+	{
+		InitializeComponent();
+	}
+
+    public async Task Button_ClickedAsync(object sender, EventArgs e)
+    {
+        //string cadena = Buscador.Text;
+        var request = new HttpRequestMessage();
+        //request.RequestUri = new Uri("https://jsonplaceholder.typicode.com/posts");
+        request.RequestUri = new Uri("https://api.blockchain.com/v3/exchange/tickers");
+        //request.RequestUri = new Uri("https://swapi.dev/api/planets/" + cadena);
+        //request.RequestUri = new Uri("https://jsonplaceholder.typicode.com/posts?userId=" + cadena);
+
+        request.Method = HttpMethod.Get;
+        request.Headers.Add("Accept", "application/json");
+
+        var client = new HttpClient();
+
+        HttpResponseMessage response = await client.SendAsync(request);
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            String content = await response.Content.ReadAsStringAsync();
+            //Dictionary<string, Post> dictionary = JsonConvert.DeserializeObject<Dictionary<string, Post>>(content);
+            //var resultado = JsonConvert.DeserializeObject<Dictionary<string, Post>>(content); 
+            var resultado = JsonConvert.DeserializeObject<List<API>>(content);
+            //var resultado = root.results;
+            ListaDemo.ItemsSource = resultado;
+
+
+        }
+    }
+}
