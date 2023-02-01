079f61dd9cced5d1842c7854f179d1ad5ea20098
David Antonio Trujillo Robalino <david.trujillo.robalino@udla.edu.ec>
2023-02-01 09:09:16 -0500

    Implementacion clases para manejar SQL lite

--- ExamenProgreso3/App.xaml.cs
+++ ExamenProgreso3/App.xaml.cs
@@ -1,8 +1,20 @@
-﻿namespace ExamenProgreso3;
+﻿using ExamenProgreso3.Data;
+
+namespace ExamenProgreso3;
 
 public partial class App : Application
 {
-	public App()
+    public static ApiDatabaseDT APIRepoDT { get; private set; }
+
+    public App(ApiDatabaseDT repo)
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+
+        APIRepoDT = repo;
+    }
+    public App()
 	{
 		InitializeComponent();
 
